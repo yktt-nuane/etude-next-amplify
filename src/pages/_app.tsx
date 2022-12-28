f60409385dc7394005f6ac9e89e932e4912dd7e0
Tatsuro Yokoyama <61369434+yktt-nuane@users.noreply.github.com>
2022-12-28 10:27:17 +0900

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,34 @@
+import { MantineProvider } from '@mantine/core'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import '@/styles/globals.css'
+
+export { reportWebVitals } from 'next-axiom'
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  )
+}
